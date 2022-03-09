b87823b1e9127de100e51d891471858fb82bcf42
qinhaojoyc <18310957475@163.com>
2022-03-09 14:55:38 +0800

    第一次提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button} from 'antd'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type={'primary'} onClick={() => alert(1)}>开始</Button>
+        <div style={{
+          width: 100,
+          background: 'red',
+          wordWrap: 'break-word'
+        }}>
+          rwerewrewrrwerewrewrrwerewrewrrwerewrewrrwerewrewrrwerewrewrrwerewrewrrwerewrewr
+        </div>
     </div>
   );
 }
